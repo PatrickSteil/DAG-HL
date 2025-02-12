49efd6e6256a546701d6ed91a6fe8ce8bfeaf7d6
PatrickSteil <patricksteil@freenet.de>
2025-02-12 14:00:55 +0100

    added licences at top

--- benchmark/label_vs_compressed_label.cpp
+++ benchmark/label_vs_compressed_label.cpp
@@ -1,14 +1,16 @@
 #include <array>
 #include <vector>
 
+/* #include "../datastructures/bit_vector.h" */
+/* #include "../datastructures/golomb_rice.h" */
 #include "../datastructures/hub_labels.h"
 #include "../external/cmdparser.hpp"
 
-void configure_parser(cli::Parser &parser) {
+void configure_parser(cli::Parser& parser) {
   parser.set_required<std::string>("i", "input_labels", "Input label file.");
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   cli::Parser parser(argc, argv);
   configure_parser(parser);
   parser.run_and_exit_if_error();
@@ -19,6 +21,8 @@ int main(int argc, char *argv[]) {
   std::size_t mem_compressed_bytes = 0;
   std::size_t mem_delta_compressed_bytes = 0;
   std::size_t mem_simple_compressed_bytes = 0;
+  /* std::size_t mem_simple_golomb_rice_bytes = 0; */
+  /* std::size_t mem_delta_golomb_rice_bytes = 0; */
 
   std::array<std::vector<Label>, 2> labels;
 
@@ -79,6 +83,60 @@ int main(int argc, char *argv[]) {
       mem_simple_compressed_bytes += comp_labels[BWD][v].byteSize();
     }
   }
+  /* { */
+  /*   GolombRice<uint32_t, uint8_t> coder(128); */
+
+  /*   std::array<std::vector<BitVector<uint8_t>>, 2> bit_vectors; */
+
+  /*   bit_vectors[FWD].reserve(labels[FWD].size()); */
+  /*   bit_vectors[BWD].reserve(labels[BWD].size()); */
+
+  /*   auto toDelta = [](const std::vector<uint32_t>& from, */
+  /*                     std::vector<uint32_t>& to) -> void { */
+  /*     to.clear(); */
+
+  /*     to.push_back(from.front()); */
+
+  /*     for (size_t i = 1; i < from.size(); ++i) { */
+  /*       to.push_back(from[i] - from[i - 1] - 1); */
+  /*     } */
+  /*   }; */
+
+  /*   for (std::size_t v = 0; v < labels[FWD].size(); ++v) { */
+  /*     std::vector<uint32_t> delta; */
+
+  /*     toDelta(labels[FWD][v].nodes, delta); */
+  /*     bit_vectors[FWD].emplace_back(coder.encode(delta)); */
+
+  /*     toDelta(labels[BWD][v].nodes, delta); */
+  /*     bit_vectors[BWD].emplace_back(coder.encode(delta)); */
+  /*   } */
+
+  /*   for (std::size_t v = 0; v < bit_vectors[FWD].size(); ++v) { */
+  /*     mem_delta_golomb_rice_bytes += bit_vectors[FWD][v].byteSize(); */
+  /*     mem_delta_golomb_rice_bytes += bit_vectors[BWD][v].byteSize(); */
+  /*   } */
+  /* } */
+
+  /* { */
+  /*   GolombRice<uint32_t, uint8_t> coder(128); */
+
+  /*   std::array<std::vector<BitVector<uint8_t>>, 2> bit_vectors; */
+
+  /*   bit_vectors[FWD].reserve(labels[FWD].size()); */
+  /*   bit_vectors[BWD].reserve(labels[BWD].size()); */
+
+  /*   for (std::size_t v = 0; v < labels[FWD].size(); ++v) { */
+  /*     bit_vectors[FWD].emplace_back(coder.encode(labels[FWD][v].nodes)); */
+  /*     bit_vectors[BWD].emplace_back(coder.encode(labels[BWD][v].nodes)); */
+  /*   } */
+
+  /*   for (std::size_t v = 0; v < bit_vectors[FWD].size(); ++v) { */
+  /*     mem_simple_golomb_rice_bytes += bit_vectors[FWD][v].byteSize(); */
+  /*     mem_simple_golomb_rice_bytes += bit_vectors[BWD][v].byteSize(); */
+  /*   } */
+  /* } */
+
   std::cout << "Memory [mb]:        " << std::endl;
   std::cout << "Raw:                " << (mem_raw_bytes / (1024 * 1024))
             << std::endl;
@@ -88,6 +146,11 @@ int main(int argc, char *argv[]) {
             << (mem_delta_compressed_bytes / (1024 * 1024)) << std::endl;
   std::cout << "Simple Compressed:  "
             << (mem_simple_compressed_bytes / (1024 * 1024)) << std::endl;
+  /* std::cout << "Delta Golomb Rice: " */
+  /*           << (mem_delta_golomb_rice_bytes / (1024 * 1024)) << std::endl; */
+  /* std::cout << "Simple Golomb Rice: " */
+  /*           << (mem_simple_golomb_rice_bytes / (1024 * 1024)) << std::endl;
+   */
 
   return 0;
 }
